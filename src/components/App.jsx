dcf66aeb2c3a1f40e63d7d7e5efbbe417c150a6c
Hack Reactor Student <student@hackreactor.com>
2018-10-01 20:32:49 -0700

    completed up to video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoCounter = 0;
+    this.state = props.state;
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <VideoList videos={exampleVideoData}/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em> { exampleVideoData.map(video => {
+              this.videoCounter++;
+              return (<VideoPlayer video={video} />);
+            })
+            } 
+            </h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> view goes here</h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+  setState() {
+    
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,35 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+// class VideoList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = props.state;
+//     this.videos = props.videos;
+//   }
+
+//   render() {
+//     console.log(this.videos);
+//     return (
+//       <div className="video-list">
+//         { this.videos.map(videos => {
+//           <VideoListEntry videos={videos} />;
+//         }) }
+//       </div>
+//     );
+//   }
+
+//   stateChange() {
+
+//   }
+// }
+var VideoList = (videosObject) => {
+  console.log(videosObject);
+  return (
+    <div className="video-list">
+      { videosObject.videos.map(video => (<VideoListEntry video={video} />)) }
+    </div>
+  );
+};
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +40,11 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+// <div className="video-list">
+//   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+// </div>);
